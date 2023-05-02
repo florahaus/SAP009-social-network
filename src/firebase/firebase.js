3cd97169787363f5235f2ccdc273817d18e97c9f
florahaus <floraahaus@gmail.com>
2023-05-02 19:52:16 -0300

    firebase-config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCd0LqsSBSLVeEISLb2poPrXfMjpgj5S1k',
+  authDomain: 'social-network-b57be.firebaseapp.com',
+  projectId: 'social-network-b57be',
+  storageBucket: 'social-network-b57be.appspot.com',
+  messagingSenderId: '801666377718',
+  appId: '1:801666377718:web:17094e55823cbf472f09d7',
+};
+
+export const app = initializeApp(firebaseConfig);
