0fd3128457a10853d74ad6f1e064d7645642e79d
florahaus <floraahaus@gmail.com>
2023-05-16 14:43:53 -0300

    criarPosts

--- src/lib/feed/feed.js
+++ src/lib/feed/feed.js
@@ -1,18 +1,39 @@
 import { auth } from '../../firebase/firebaseLogin.js';
+import { addPost } from '../../firebase/firestore.js';
 
 export default () => {
   const containerFeed = document.createElement('div');
   const templateFeed = `
   <link rel="stylesheet" href="./lib/feed/feed.css">
+
     <h2 id="titFeed"> Registre suas memórias favoritas</h2>
+
     <form class="formFeed">
-     <label id="postFeed" for="postagens"> Post:</label>
-     <input id="inputFeed" type="textarea" name="postagem">
-  
+     <label id="postFeed" for="inputFeed"> Post:</label>
+     <input id="inputFeed" type="textarea" name="postagem" placeholder="escreva sua mensagem para o futuro ou sua memória favorita">
+     </form>
+
      <input id="botaoFeed" type="submit" value="Enviar">
-    </form>
+   <section id="tdsPosts">
+   <ul id="feedComPosts"></ul>
+   </section>
+
     `;
+
   containerFeed.innerHTML = templateFeed;
   console.log(auth.currentUser.displayName);
+
+  const ConteudoPost = containerFeed.querySelector('#inputFeed');
+  const botaoFeed = containerFeed.querySelector('#botaoFeed');
+
+  botaoFeed.addEventListener('click', (event) => {
+    event.preventDefault();
+    // função para adicionar post a coleção;
+    addPost(ConteudoPost.value, auth.currentUser.displayName, new Date())
+      .then(() => {
+        console.log(ConteudoPost.value);
+      })
+      .catch((error) => error);
+  });
   return containerFeed;
 };
--- src/lib/login/login.js
+++ src/lib/login/login.js
@@ -1,29 +1,42 @@
-import { login } from '../../firebase/firebaseLogin.js';
+import {
+  login,
+  loginGoogle,
+} from '../../firebase/firebaseLogin.js';
+import google from '../../imagens/google.png';
 
 export default () => {
   const container = document.createElement('div');
   const template = `
-  <link rel="stylesheet" href="./lib/login/login.css">
   <h2 class="titLogin"> Faça o login ou cadastre-se </h2>
 
   <form class="formLogin">
-    <label id="labelEmail" for="email"> E-mail:</label>
+    <label id="labelEmail" for="inputEmail"> E-mail:</label>
     <input type="email" id="inputEmail" name="email" placeholder="email@email.com">
 
-    <label id="labelPassword" for="password"> Senha:</label>
-    <input for="password" type="password" id="inputPassword" name="password" placeholder="********">
+    <label id="labelPassword" for="inputPassword"> Senha:</label>
+    <input type="password" id="inputPassword" name="password" placeholder="********">
 
     <button id="botaoLogin" type="submit" value="Enviar"> Enviar </button>
-  </form>
+  </form> 
+
+  <section class="popUp">
+  <p id="pPopUp">Login com</p>
+  <button id="popUpGoogle" type="button">
+    <img src="${google}" id="iconeGoogle" alt="icone google">
+  </button>
+</section>
 
   <p id="pCadLogin"> Ainda não possui conta? <a id="linkCadLogin" href="/#cadastro">Cadastre-se </a></p>
+<link rel="stylesheet" href="./lib/login/login.css"></link>
   `;
+
   container.innerHTML = template;
 
   const email = container.querySelector('#inputEmail');
   const password = container.querySelector('#inputPassword');
-
   const botaoLogin = container.querySelector('#botaoLogin');
+  const btnPopUp = container.querySelector('#popUpGoogle');
+
   botaoLogin.addEventListener('click', (event) => {
     event.preventDefault();
     login(email.value, password.value)
@@ -35,5 +48,15 @@ export default () => {
         error.classList.remove('hidden');
       });
   });
+
+  btnPopUp.addEventListener('click', (e) => {
+    e.preventDefault();
+    loginGoogle()
+      .then(() => {
+        window.location.hash = '#feed';
+      })
+      .catch((error) => error);
+  });
+
   return container;
 };
