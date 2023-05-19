3dfcb264bbea031a0905f5a020f4333f297f4758
florahaus <floraahaus@gmail.com>
2023-05-19 09:49:02 -0300

    mostrarPosts

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -2,6 +2,9 @@ import {
   getFirestore,
   collection,
   addDoc,
+  query,
+  onSnapshot,
+  orderBy,
 } from 'firebase/firestore';
 import { app } from './firebase.js';
 
@@ -15,3 +18,12 @@ export const addPost = async (conteudo, displayName, data) => {
   });
   return addPost;
 };
+
+export function mostrarPosts(infoFeed) {
+  const q = query(collection(db, 'posts'), orderBy('data', 'desc'));
+  onSnapshot(q, (querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      infoFeed(doc.data());
+    });
+  });
+}
